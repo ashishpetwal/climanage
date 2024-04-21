3bbf555b460ce4765a9f8e77dbc9ca03976d2a9c
Ashish <ashishpetwalap2112@gmail.com>
2024-04-21 21:46:24 +0530

    Minor UI fixes

--- app/agency/page.js
+++ app/agency/page.js
@@ -22,7 +22,7 @@ const Agency = () => {
                 <div className="absolute -z-[10] bg-gradient-to-r rounded-full from-[#C8FC7433] to-[#8673A433] blur-2xl w-[70%] h-[70%]"></div>
                 <div className="absolute bottom-0 right-0 -z-[10] bg-gradient-to-r rounded-full from-[#c0e38980] to-[#9db47980] blur-2xl w-[50%] h-[40%]"></div>
                 <Header />
-                <section className="grid grid-cols-1 px-4">
+                <section className="grid grid-cols-1 gap-4 p-4">
                     <div className="flex flex-col md:flex-row gap-4 md:gap-0 justify-between p-4">
                         <h2 className="text-2xl md:texts-3xl font-medium">All Agencies/Freelancers</h2>
                         <div className="flex gap-2 items-center">
@@ -37,60 +37,56 @@ const Agency = () => {
                             <button onClick={() => { if (currentItem !== pages.length - 1) { setCurrentItem(currentItem + 1) } }} className="bg-[#736589] text-white p-1 rounded-md"><IoIosArrowForward /></button>
                         </div>
                     </div>
-                    <div className="flex flex-col justify-between items-start lg:flex-row lg:items-center p-4 gap-4">
-                        <div className="flex gap-4 items-center">
-                            <div className="flex p-2.5 rounded-lg bg-gray-400 gap-2">
-                                <div>
-                                    <form className="max-w-24 mx-auto">
-                                        <select id="options" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5">
-                                            <option selected>View All</option>
-                                            <option value="1">Option 1</option>
-                                            <option value="2">Option 2</option>
-                                            <option value="3">Option 3</option>
-                                            <option value="4">Option 4</option>
-                                        </select>
-                                    </form>
-                                </div>
-                                <div>
-                                    <form className="max-w-24 mx-auto">
-                                        <select id="options" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5">
-                                            <option selected>Action</option>
-                                            <option value="1">Option 1</option>
-                                            <option value="2">Option 2</option>
-                                            <option value="3">Option 3</option>
-                                            <option value="4">Option 4</option>
-                                        </select>
-                                    </form>
-                                </div>
-                                <div>
-                                    <form className="max-w-24 mx-auto">
-                                        <select id="options" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5">
-                                            <option selected>Filter by</option>
-                                            <option value="1">Option 1</option>
-                                            <option value="2">Option 2</option>
-                                            <option value="3">Option 3</option>
-                                            <option value="4">Option 4</option>
-                                        </select>
-                                    </form>
-                                </div>
+                    <div className="flex flex-col lg:flex-row gap-4 justify-between px-4">
+                        <div className="flex justify-between gap-2">
+                            <div className="flex justify-center items-center bg-[#d9d9d9] p-1 rounded-lg gap-2">
+                                <form className="">
+                                    <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5">
+                                        <option selected>View All</option>
+                                        <option value="1">Option 1</option>
+                                        <option value="2">Option 2</option>
+                                        <option value="3">Option 3</option>
+                                        <option value="4">Option 4</option>
+                                    </select>
+                                </form>
+                                <form className="">
+                                    <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5">
+                                        <option selected>Action</option>
+                                        <option value="1">Option 1</option>
+                                        <option value="2">Option 2</option>
+                                        <option value="3">Option 3</option>
+                                        <option value="4">Option 4</option>
+                                    </select>
+                                </form>
+                                <form className="">
+                                    <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5">
+                                        <option selected>Filter by</option>
+                                        <option value="1">Option 1</option>
+                                        <option value="2">Option 2</option>
+                                        <option value="3">Option 3</option>
+                                        <option value="4">Option 4</option>
+                                    </select>
+                                </form>
                             </div>
-                            <button type="button" className="flex items-center text-nowrap gap-1 p-4 text-sm text-center text-white bg-[#736589] rounded-lg">Add New<IoMdAdd className="text-xl hidden md:block" /></button>
+                            <div>
+                                <button className="bg-[#736589] text-nowrap h-full inline-flex px-2 gap-1 items-center text-white rounded-md">Add New <span className="hidden lg:inline"><IoMdAdd /></span></button>
+                            </div>
                         </div>
-                        <div className="flex flex-col mx-auto md:mx-0 justify-center gap-2 md:gap-4 lg:gap-2 items-start lg:flex-row lg:justify-end lg:items-center">
-                            <div className="flex gap-2">
-                                <button type="button" className="text-green-700 text-nowrap hover:text-white border border-green-700 hover:bg-green-800 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm p-2 md:p-4 text-center">Active (53)</button>
-                                <button type="button" className="text-[#813E0E] text-nowrap hover:text-white border border-[#813E0E] hover:bg-red-800 focus:outline-none font-medium rounded-lg text-sm p-2 md:p-4 text-center">Inactive (53)</button>
-                                <button type="button" className="text-red-500 text-nowrap hover:text-white border border-red-500 hover:bg-red-500 focus:outline-none font-medium rounded-lg text-sm p-2 md:p-4 text-center">Trashed (53)</button>
+                        <div className="flex flex-col lg:flex-row gap-4 lg:gap-1.5">
+                            <div className="flex justify-between gap-1.5">
+                                <button className="text-nowrap h-full px-1 md:px-4 py-[8.8px] text-[#30B26C] rounded-md border border-[#13A757]">Active (53)</button>
+                                <button className="text-nowrap h-full px-1 md:px-4 py-[8.8px] text-[#813E0E] rounded-md border border-[#813E0E]">Inactive (53)</button>
+                                <button className="text-nowrap h-full px-1 md:px-4 py-[8.8px] text-[#FE493C] rounded-md border border-[#FE493C]">Trashed (53)</button>
                             </div>
-                            <form class="w-full lg:w-1/3">
-                                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
-                                <div class="relative">
-                                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                        <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <form className="">
+                                <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+                                <div className="relative">
+                                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                        <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                         </svg>
                                     </div>
-                                    <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 rounded-xl bg-[#e9e9e9] outline-none" placeholder="Search..." required />
+                                    <input type="search" id="default-search" className="block w-full p-2.5 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-[#E9E9E9]" placeholder="Search..." required />
                                 </div>
                             </form>
 
@@ -99,7 +95,7 @@ const Agency = () => {
                 </section>
                 <div className="grid gap-4 grid-cols-1 px-4">
                     <Table />
-                    <div className="flex gap-2 px-4 mb-4 items-center justify-start md:justify-end">
+                    <div className="flex gap-2 px-4 mb-12 items-center justify-start md:justify-end">
                         <button onClick={() => { if (currentItem !== 0) { setCurrentItem(currentItem - 1) } }} className="bg-[#736589] text-white p-1 rounded-md"><IoIosArrowBack /></button>
                         <div>
                             {
--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -6,81 +6,81 @@ const Table = () => {
   ];
 
   return (
-    <div class="flex flex-col p-4">
-      <div class="-m-1.5 overflow-x-auto">
-        <div class="p-1.5 min-w-full inline-block align-middle">
-          <div class="border rounded-lg overflow-hidden">
-            <table class="min-w-full">
-              <thead class="bg-[#e3e3e3]">
+    <div className="flex flex-col p-4">
+      <div className="-m-1.5 overflow-x-auto">
+        <div className="p-1.5 min-w-full inline-block align-middle">
+          <div className="border rounded-lg overflow-hidden">
+            <table className="min-w-full">
+              <thead className="bg-[#e3e3e3]">
                 <tr>
-                  <th scope="col" class="py-3 ps-4">
-                    <div class="flex items-center h-5">
+                  <th scope="col" className="py-3 ps-4">
+                    <div className="flex items-center h-5">
                       <input
                         id="hs-table-checkbox-all"
                         type="checkbox"
-                        class="border-gray-200 rounded text-blue-600 focus:ring-blue-500"
+                        className="border-gray-200 rounded text-blue-600 focus:ring-blue-500"
                       />
-                      <label for="hs-table-checkbox-all" class="sr-only">
+                      <label for="hs-table-checkbox-all" className="sr-only">
                         Checkbox
                       </label>
                     </div>
                   </th>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-start text-sm font-medium text-black"
+                    className="px-6 py-3 text-start text-sm font-medium text-black"
                   >
                     Name
                   </th>
                   <th
                     scope="col"
-                    class="hidden md:table-cell px-6 py-3 text-start text-sm font-medium text-black"
+                    className="hidden md:table-cell px-6 py-3 text-start text-sm font-medium text-black"
                   >
                     Subscription
                   </th>
                   <th
                     scope="col"
-                    class="hidden md:table-cell px-6 py-3 text-start text-sm font-medium text-black"
+                    className="hidden md:table-cell px-6 py-3 text-start text-sm font-medium text-black"
                   >
                     Profile Status
                   </th>
                   <th
                     scope="col"
-                    class="hidden md:table-cell px-6 py-3 text-start text-sm font-medium text-black"
+                    className="hidden md:table-cell px-6 py-3 text-start text-sm font-medium text-black"
                   >
                     Number of Clients
                   </th>
                   <th
                     scope="col"
-                    class="hidden md:table-cell px-6 py-3 text-start text-sm font-medium text-black"
+                    className="hidden md:table-cell px-6 py-3 text-start text-sm font-medium text-black"
                   >
                     Registered On
                   </th>
                 </tr>
               </thead>
-              <tbody class="bg-white divide-y divide-[#606060]">
+              <tbody className="bg-white divide-y divide-[#6060604f]">
                 {agencies.map((index) => {
                   return (
                     <tr key={index}>
-                      <td class="py-3 ps-4">
-                        <div class="flex items-center h-5">
+                      <td className="py-3 ps-4">
+                        <div className="flex items-center h-5">
                           <input
                             id="hs-table-checkbox-1"
                             type="checkbox"
-                            class="border-gray-200 rounded text-blue-600 focus:ring-blue-500"
+                            className="border-gray-200 rounded text-blue-600 focus:ring-blue-500"
                           />
-                          <label for="hs-table-checkbox-1" class="sr-only">
+                          <label for="hs-table-checkbox-1" className="sr-only">
                             Checkbox
                           </label>
                         </div>
                       </td>
-                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
                         <div className="flex items-center gap-4">
                           <Image
                             src="/staredo.png"
                             width={100}
                             height={100}
                             alt="avatar"
-                            class="w-12 h-12 rounded-lg"
+                            className="w-12 h-12 rounded-lg"
                           />
                           <span>
                             <p className="text-[#141414] pr-4 text-base text-wrap">
@@ -92,7 +92,7 @@ const Table = () => {
                           </span>
                         </div>
                       </td>
-                      <td class="hidden md:table-cell px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                      <td className="hidden md:table-cell px-6 py-4 whitespace-nowrap text-sm text-gray-800">
                         <span>
                           <p className="text-[#141414] text-base font-medium">
                             Advanced Plan
@@ -102,7 +102,7 @@ const Table = () => {
                           </p>
                         </span>
                       </td>
-                      <td class="hidden md:table-cell px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                      <td className="hidden md:table-cell px-6 py-4 whitespace-nowrap text-sm text-gray-800">
                         <span className="space-y-2">
                           <span className="text-xs py-2 px-3 rounded-full bg-[#B7FB49]">
                             Active
@@ -115,15 +115,15 @@ const Table = () => {
                           </p>
                         </span>
                       </td>
-                      <td class="hidden md:table-cell px-6 py-4 whitespace-nowrap text-start text-sm font-medium">
+                      <td className="hidden md:table-cell px-6 py-4 whitespace-nowrap text-start text-sm font-medium">
                         <button
                           type="button"
-                          class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none"
+                          className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none"
                         >
                           55
                         </button>
                       </td>
-                      <td class="hidden md:table-cell px-6 py-4 whitespace-nowrap text-start text-sm text-gray-800">
+                      <td className="hidden md:table-cell px-6 py-4 whitespace-nowrap text-start text-sm text-gray-800">
                         <span>
                           <p className="text-[#141414] text-base font-medium">
                             Apr 9 2024
